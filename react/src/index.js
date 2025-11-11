687f36fb48d4ae3b15f1443abc9171868bb1ad99
IbragimKhaniev <newinterny@gmail.com>
2025-11-11 10:32:07 +0300

    feat(frontend): add Ant Design, icons, Dayjs; set Russian locale and wrap App with BrowserRouter and ConfigProvider

--- react/src/index.js
+++ react/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'antd/dist/reset.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+import ru_RU from 'antd/locale/ru_RU';
+import { BrowserRouter } from 'react-router-dom';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+
+// Set Dayjs locale to Russian
+dayjs.locale('ru');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ConfigProvider locale={ru_RU}>
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
